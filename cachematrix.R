acf6ccdcd2b61d837fa0473bc473110e2b4d0aad
Ashi <wykape@gmail.com>
2021-01-07 22:02:05 +0800

    Coursera Assignment 2, edited.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## Following the blueprint code of MakeVector, the following function
+## allows the user to store and retrieve the value of a matrix and its inverse
+   ## Default matrix taken from sample matrix that in the Coursera forum
 
+makeCacheMatrix <- function(mxnow = matrix(c(1/2, -1/4, -1, 3/4), 2, 2)) {
+  inv <- NULL
+  
+  setmx <- function (newmx){
+    mxnow <<- newmx
+    inv <<- NULL
+  ## setmx takes a matrix argument and sets it as the new matrix, erasing the cached
+  ## inverse of the previous matrix
+  } 
+
+  getmx <- function(){
+    mxnow
+  }
+  ## getmx, when called, simply allows the user to view the currently stored matrix
+  
+  setinv <- function(newinv){
+    inv <<- newinv
+  }
+  ## A new inverse now exists in the function's environment
+
+  getinv <- function(){
+    inv
+  }
+  
+  list(setmx = setmx, getmx = getmx,
+       setinv = setinv,
+       getinv = getinv)
+  ## Function makeCacheMatrix returns a list through which an object can call its functions
+  ## by simply using the $ operator ie. newObject$getinv()
 }
+##___________________________________________________________________________________
 
+## The following function calculates the inverse of the matrix or retrieves the previously
+## cached inverse from the makeCacheMatrix object, if present 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cacheSolve <- function(mxobj, ...) {
+  inv <- mxobj$getinv()
+    ##Load up inverse from makeCacheMatrix object 
+  if(!is.null(inv)){
+      return(inv)
+  }
+    ## If returned inverse is not null, ie. already cached, then simply return cached inverse 
+  getnewmx <- mxobj$getmx()
+    ## If no inverse found, start computing for new inverse by retrieving saved matrix
+  inv <- solve(getnewmx, ...)
+    ## Compute for inverse of matrix using solve() function
+  mxobj$setinv(inv)
+    ## Save or cache newly computed matrix inverse
+  inv
+  }
